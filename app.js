f8970e50575b1b8c36d1057775dd2c6dfd9632ce
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 16:35:15 -0300

    se crean estructuras basicas

---
+++ app.js
@@ -0,0 +1,7 @@
+/*Crea una función que compruebe si un número es par o impar.
+
+Sigue los pasos a continuación para que completes el programa.
+
+Implementa una función comprobar(n). Nota: Puedes usar el operador modulo: %.
+Si el número es par retornar 0.
+Si el número es impar retornar 1.*/
