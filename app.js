268303dcd89bc7ea2907eeef2fd94be9f44e3cba
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 16:37:23 -0300

    se crea funcion comprobar

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ Sigue los pasos a continuación para que completes el programa.
 Implementa una función comprobar(n). Nota: Puedes usar el operador modulo: %.
 Si el número es par retornar 0.
 Si el número es impar retornar 1.*/
+function comprobar(n){
+    if(n % 2 == 0) {//se da condicion para que retorne par
+        return "par";
+      }
+      else {
+        return "impar";//si modulo de n es distinto de 0, será impar
+      }
+    };
